35f0d517fb301529a1c9e208b237c9fde7db4026
浮生多不虞 <fushengduobuyu@163.com>
2023-06-07 01:45:15 +0800

    test:初始化jest来测试socketIO

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,23 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+const socket = require('socket.io-client');
 
-import { HomeComponent } from './home.component';
-
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+describe("Test the chat socket in home ", () => {
+  it("should connect to the server", (done) => {
+    console.log(socket)
   });
 });
